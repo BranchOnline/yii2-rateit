8640ce0fdd616d6bec911b0caa00e29ba4f5192d
Jap <jap@branchonline.nl>
2016-01-04 16:54:16 +0100

    Update Rateit.php

--- src/Rateit.php
+++ src/Rateit.php
@@ -7,90 +7,76 @@ use yii\widgets\InputWidget;
 
 /**
  * The Rateit widget is a wrapper for the jquery rateit library.
- * 
+ *
  * Usage:
  * echo branchonline\rateit\Rateit::widget([
  *     'name' => 'my-rateit-field',
  *     'value' => 4, //optional ofcoure
  * ]);
  *
- * @author Jap Mul
+ * @author Jap Mul <jap@branchonline.nl>
  */
 class Rateit extends InputWidget {
 
-    /**
-     * 
-     * @var integer the step value 
-     */
+    /** @var integer The step value. */
     public $step = 1;
-    
-    /**
-     *
-     * @var integer the minimum value
-     */
+
+    /** @var integer The minimum value. */
     public $min = 0;
-    
-    /**
-     *
-     * @var integer the maximum value
-     */
+
+    /** @var integer The maximum value. */
     public $max = 10;
-    
-    /**
-     *
-     * @var boolean when not readonly, whether to show the reset button
-     */
+
+    /** @var boolean When not readonly, whether to show the reset button. */
     public $resetable = false;
-    
-    /**
-     *
-     * @var integer width of the (star) picture
-     */
+
+    /** @var integer Width of the (star) picture. */
     public $star_width = 16;
-    
-    /**
-     *
-     * @var integer height of the (star) picture
-     */
+
+    /** @var integer Height of the (star) picture. */
     public $star_height = 16;
-    
-    /**
-     *
-     * @var string optional extra class to be added to the rateit div.
-     */
+
+    /** @var string Optional extra class to be added to the rateit div. */
     public $rateit_class = null;
-    
+
     /**
      * Initializes the widgets. Registers the assets.
+     * @return void
      */
     public function init() {
         parent::init();
-        
+
         $this->options['type'] = 'range';
         $this->options['step'] = $this->step;
-        
+
         RateitAsset::register($this->getView());
     }
 
     /**
      * Runs the widgets. Renders the input field and the rateit div.
+     * @return void
      */
     public function run() {
         $id = $this->getId();
-        if($this->hasModel()) {
+        if ($this->hasModel()) {
+            $this->options['id'] = $id;
+            if (empty($this->model->{$this->attribute})) {
+                $this->model->{$this->attribute} = (int) ($this->max / 2);
+            }
             echo Html::activeTextInput($this->model, $this->attribute, $this->options);
         } else {
             echo Html::textInput($this->name, $this->value, $this->options);
         }
-        
+
         echo Html::tag('div', null, [
-            'class' => 'rateit ' . $this->rateit_class,
+            'class'                  => 'rateit ' . $this->rateit_class,
             'data-rateit-backingfld' => '#' . $id,
-            'data-rateit-resetable' => $this->resetable ? 'true' : 'false',
-            'data-rateit-min' => $this->min,
-            'data-rateit-max' => $this->max,
-            'data-rateit-starwidth' => $this->star_width,
+            'data-rateit-max'        => $this->max,
+            'data-rateit-min'        => $this->min,
+            'data-rateit-resetable'  => ($this->resetable) ? 'true' : 'false',
             'data-rateit-starheight' => $this->star_height,
+            'data-rateit-starwidth'  => $this->star_width,
         ]);
     }
+
 }
